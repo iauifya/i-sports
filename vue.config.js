0712d4876ba4239b9581b5e9731fdec849762069
iauifya <iauifya@gmail.com>
2022-07-15 17:14:20 +0800

    vue.config更新

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false
 })
+module.exports = { 
+  publicPath: process.env.NODE_ENV === 'production' 
+  ? '/i-sports/' //為github的專案名稱 
+  : '/' 
+ }
